c484457d9b70859cc8a20d4da4e5fdb402196c28
nogodan1234 <choi.taeho@gmail.com>
2019-07-09 14:55:25 +1000

    adding while loop for AOS pkg

--- nutanix_home_clean.sh
+++ nutanix_home_clean.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/bash
 
-echo "Removing files from safe directory to delete"
+echo "Removing files from delete safe directory "
 rm -rf /home/nutanix/data/cores/*
 rm -rf /home/nutanix/data/binary_logs/*
 rm -rf /home/nutanix/data/ncc/installer/*
 rm -rf /home/nutanix/data/log_collector/*
 rm -rf /home/nutanix/foundation/tmp/*
 
+echo "Checking Prsim service temp directory"
+for i in `svmips` ; do echo $i ; ssh $i du -sh /home/nutanix/prism/temp  /dev/null | head -n 2 ; done
+#allssh "du -sh /home/nutanix/prism/temp"
+
+echo "Checking for old AOS installer pkg"
+CURRENT_VERSION=`ncli cluster info | grep "Cluster Version" | awk '{print $4}'`
+echo "current AOS version is $CURRENT_VERSION"
+
+NUM_OLDAOS=`ls -latr /home/nutanix/data/installer/ | grep -v $CURRENT_VERSION | grep release-euphrates | awk '{print $9}'|wc -l`
+echo "$NUM_OLDAOS old AOS pkg detected"
+while [ $NUM_OLDAOS -gt 0 ]  
+do
+        OLD_AOSVER=`ls -ltr /home/nutanix/data/installer/ | grep -v $CURRENT_VERSION | grep release-euphrates | awk '{print $9}'`
+        echo "Removing Old AOS version"
+        rm -rf /home/nutanix/data/installer/$OLD_AOSVER
+        NUM_OLDAOS=$[$NUM_OLDAOS-1]
+done
+
+#checking for NCC 3.7.0 bug
+echo "Checking for NCC bug ENG-220802(big big ncc_log_collector.log) "
+#allssh "ls -lSh ~/data/logs/ncc*.log | head -n 2"
+for i in `svmips` ; do echo $i ; ssh $i ls -lSh ~/data/logs/ncc*.log  /dev/null | head -n 2 ; done
+
 echo "Listing foundation image iso from /home/nutanix/foundation/isos/ "
 find /home/nutanix/foundation/isos/ -type f -size +100M -exec ls -l {} \;
 #find /home/nutanix/foundation/isos/ -type f -size +100M -exec rm {} \;
 echo "Please check to see if any file can be deleted"
-sleep 15 
+sleep 15
 
 echo "Older than 4 day log files from ~/data/logs/"
 find /home/nutanix/data/logs -type f -mtime +4 -exec ls -l {} \;
 #find /home/nutanix/data/logs -type f -mtime +4 -exec rm {} \;
 echo "Please check to see if any file can be deleted"
 sleep 15
-
-echo "Checking Prsim service temp directory"
-allssh "du -sh /home/nutanix/prism/temp"
-
-echo "Checking for old AOS install pkg"
-CURRENT_VERSION=`ncli cluster info | grep "Cluster Version" | awk '{print $4}'`
-echo "current AOS version is $CURRENT_VERSION"
-ls -latr /home/nutanix/data/installer/ | grep -v $CURRENT_VERSION | grep release | awk '{print $9}'
-OLD_AOSVER=`ls -latr /home/nutanix/data/installer/ | grep -v $CURRENT_VERSION | grep release | awk '{print $9}'`
-echo "Old AOS pkg $OLD_AOSVER is detected"
-
-echo "Removing Old AOS version"
-rm -rf /home/nutanix/data/installer/$OLD_AOSVER
-
-#checking for NCC 3.7.0 bug
-echo "Checking for NCC bug ENG-220802(big big ncc_log_collector.log) "
-allssh "ls -lSh ~/data/logs/ncc*.log | head -n 2"
