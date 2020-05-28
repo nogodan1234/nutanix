94d23d88bc10fdc0e8efe7dc2d6996bf986d421c
nogodan1234 <choi.taeho@gmail.com>
2020-05-28 20:15:09 +1000

    Adding fw check sh

---
+++ Nutanix_fw_check.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+#This is for checking bios firmware version
+#28 May 2020 taeho.choi@nutanix.com
+# 
+
+echo "########################################################"
+echo " What is the hypervisor type? 1.AHV 2.ESXi 3.Hyper-V "
+echo "########################################################"
+read HYPERVISOR
+
+if [ $HYPERVISOR = "1" ]
+then
+	echo "You selected AHV ... checking the detail.."
+	echo "########################################################"
+	echo "HW Model is  "
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i 'dmidecode | grep "Product Name" '| head -1 ;done
+	echo "########################################################"
+	echo "BMC version is "
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i "ipmitool mc info| grep 'Firmware Revision'"|cut -d ' ' -f12 ;done
+	echo "########################################################"
+	echo "BIOS version is"
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i "dmidecode | egrep 'Version: 3|Version: 4'" | cut -d ' ' -f2 ;done
+
+elif [ $HYPERVISOR = "2" ]
+then
+	echo "You selected ESXi ... checking the detail.."
+	echo "########################################################"
+	echo "HW Model is  "
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i  smbiosDump | grep -i product| head -1 | awk -F' ' '{print $2}'; done
+	echo "########################################################"
+	echo "BMC version is "
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i "/ipmitool bmc info| grep -i firmware "|cut -d ' ' -f12  ;done
+	echo "########################################################"	
+	echo "BIOS version is"
+	echo "########################################################"
+	for i in `hostips` ; do echo "==== $i ====" ; ssh -q root@$i "smbiosDump  | grep Version | head -1" | cut -d ' ' -f6 ;done
+
+elif [ $HYPERVISOR = "3" ]
+then
+	echo "You selected Hyper-V ... checking the detail.."
+	echo "########################################################"
+	echo "HW Model is  "
+	echo "########################################################"
+	hostssh 'systeminfo| grep "System Model" '
+	echo "########################################################"	
+	echo "BMC version is "
+	echo "########################################################"
+	hostssh date
+	hostssh 'ipmiutil sel -v | grep BMC ' | cut -d ' ' -f4
+	echo "########################################################"	
+	echo "BIOS version is"
+	echo "########################################################"
+	hostssh 'systeminfo | grep BIOS' | awk '{print $6}'
+else
+	echo "You selected wrong option"
+fi
+
+#BMC version
+#ncli host ls | grep "IPMI Address" | awk -F: '{print $2}' | while read LINE ; do echo IPMI address $LINE ; ipmitool -I lanplus -H $LINE -U ADMIN -P ADMIN  bmc info | grep "Firmware Revision" ; done
